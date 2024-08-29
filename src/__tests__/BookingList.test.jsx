59a955714d5781a80f7a22cc958d14aa07a3fb67
Diana <dianadorojuc3@gmail.com>
2024-08-29 16:29:30 +0200

    commit input testing

--- src/__tests__/BookingList.test.jsx
+++ src/__tests__/BookingList.test.jsx
@@ -1,7 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookList from "../components/BookList";
 import history from "../assets/books/history.json";
-
 
 //Verifico che vengano effettivamente renderizzate tante bootstrap cards quanti sono i libri nel file json utilizzato.
 describe("Check if as many bootstrap cards are rendered as there are books in the json file", () => {
@@ -13,5 +12,19 @@ describe("Check if as many bootstrap cards are rendered as there are books in th
     const cardImage = await screen.findAllByRole("img");
     //controllo la lunghezza dell'array
     expect(cardImage.length).toBeGreaterThan(0);
+  });
+
+  //Verifico che il filtraggio dei libri tramite navbar
+  it("filters books based on input placeholder", () => {
+    render(<BookList libri={history} />);
+
+    // Trovo l'input della barra di ricerca
+    const myPlaceholder = screen.getByPlaceholderText(/cerca titolo libro/i);
+
+    // Filtra con un libro che non corrisponde a nessun libro del mio json
+    fireEvent.change(myPlaceholder, { target: { value: "Nonexistent Book" } });
+
+    const noFind = screen.queryAllByText(/the human story/i);
+    expect(noFind.length).toBe(0);
   });
 });
